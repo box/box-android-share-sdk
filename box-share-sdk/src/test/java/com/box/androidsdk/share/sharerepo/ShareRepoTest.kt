b9b94be8217d37724bc6a74a6c584d23a6a4df1b
myothiha09 <myothiha1997@gmail.com>
2019-07-22 13:58:10 -0700

    updated share repo test to include new methods

--- box-share-sdk/src/test/java/com/box/androidsdk/share/sharerepo/ShareRepoTest.kt
+++ box-share-sdk/src/test/java/com/box/androidsdk/share/sharerepo/ShareRepoTest.kt
@@ -2,19 +2,18 @@ package com.box.androidsdk.share.sharerepo
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.box.androidsdk.content.BoxFutureTask
-import com.box.androidsdk.content.models.BoxCollaboration
-import com.box.androidsdk.content.models.BoxCollaborationItem
-import com.box.androidsdk.content.models.BoxItem
-import com.box.androidsdk.content.requests.BoxRequestsFile
-import com.box.androidsdk.content.requests.BoxResponse
-import com.box.androidsdk.content.requests.BoxResponseBatch
+import com.box.androidsdk.content.models.*
+import com.box.androidsdk.content.requests.*
 import com.box.androidsdk.share.api.ShareController
+import com.box.androidsdk.share.internal.models.BoxFeatures
 import com.box.androidsdk.share.internal.models.BoxIteratorInvitees
 import com.nhaarman.mockitokotlin2.*
 import junit.framework.Assert.*
 
 import org.junit.*
 import org.junit.rules.TestRule
+import java.lang.Exception
+import java.lang.IllegalArgumentException
 import java.util.*
 
 class ShareRepoTest {
@@ -29,8 +28,13 @@ class ShareRepoTest {
     private val mockEmailList: Array<String> = arrayOf("boxuser@box.com", "boxuser2@box.com")
     private val mockSelectedRole: BoxCollaboration.Role = BoxCollaboration.Role.EDITOR
     private val mockFilter: String = "filter"
+    private val mockCollaboration: BoxCollaboration = mock()
 
-    private val canDownload = false;
+    private val mockBoxFile: BoxFile = mock()
+
+    private val canDownload = false
+    private val newAccess = BoxSharedLink.Access.COMPANY
+    private val newPassword = ""
 
     private val mockGetInviteeResponseTask: BoxFutureTask<BoxIteratorInvitees> = mock()
     private val mockGetInviteeResponse: BoxResponse<BoxIteratorInvitees> = mock()
@@ -38,9 +42,26 @@ class ShareRepoTest {
     private val mockFetchRolesResponse: BoxResponse<BoxCollaborationItem> = mock()
     private val mockAddCollabsResponseTask: BoxFutureTask<BoxResponseBatch> = mock()
     private val mockAddCollabsResponse: BoxResponse<BoxResponseBatch> = mock()
+    private val mockFetchItemInfoResponseTask: BoxFutureTask<BoxItem> = mock()
+    private val mockFetchItemInfoResponse: BoxResponse<BoxItem> = mock()
 
-    private val mockSharedLinksOperationResponseTask: BoxFutureTask<BoxItem> = mock()
-    private val mockSharedLinksOperationResponse: BoxResponse<BoxItem> = mock()
+    private val mockSharedLinkResponseTask: BoxFutureTask<BoxItem> = mock()
+    private val mockSharedLinkResponse: BoxResponse<BoxItem> = mock()
+
+    private val mockFeatureResponseTask: BoxFutureTask<BoxFeatures> = mock()
+    private val mockFeatureResponse: BoxResponse<BoxFeatures> = mock()
+
+    private val mockDeleteCollaborationResponseTask: BoxFutureTask<BoxVoid> = mock()
+    private val mockDeleteCollaborationResponse: BoxResponse<BoxVoid> = mock()
+
+    private val mockUpdateOwnerResponseTask: BoxFutureTask<BoxVoid> = mock()
+    private val mockUpdateOwnerResponse: BoxResponse<BoxVoid> = mock()
+
+    private val mockUpdateCollaborationResponseTask: BoxFutureTask<BoxCollaboration> = mock()
+    private val mockUpdateCollaborationResponse: BoxResponse<BoxCollaboration> = mock()
+
+    private val mockFetchCollaborationResponseTask: BoxFutureTask<BoxIteratorCollaborations> = mock()
+    private val mockFetchCollaborationResponse: BoxResponse<BoxIteratorCollaborations> = mock()
 
     private val mockDate: Date = mock()
 
@@ -48,11 +69,30 @@ class ShareRepoTest {
 
     @Before
     fun setup() {
+        whenever(shareController.fetchItemInfo(mockShareItem)).thenReturn(mockFetchItemInfoResponseTask)
+
         whenever(shareController.getInvitees(mockShareItem, mockFilter)).thenReturn(mockGetInviteeResponseTask)
         whenever(shareController.fetchRoles(mockShareItem)).thenReturn(mockFetchRolesResponseTask)
         whenever(shareController.addCollaborations(mockShareItem, mockSelectedRole, mockEmailList)).thenReturn(mockAddCollabsResponseTask)
-        whenever(shareController.createDefaultSharedLink(mockShareItem)).thenReturn(mockSharedLinksOperationResponseTask)
-        whenever(shareController.disableShareLink(mockShareItem)).thenReturn(mockSharedLinksOperationResponseTask)
+
+        whenever(shareController.createDefaultSharedLink(mockShareItem)).thenReturn(mockSharedLinkResponseTask)
+        whenever(shareController.disableShareLink(mockShareItem)).thenReturn(mockSharedLinkResponseTask)
+
+        val mockBoxRequestsFile: BoxRequestsFile.UpdatedSharedFile = mock()
+        whenever(shareController.getCreatedSharedLinkRequest(mockShareItem)).thenReturn(mock())
+        whenever(shareController.getCreatedSharedLinkRequest(mockBoxFile)).thenReturn(mockBoxRequestsFile)
+        whenever(shareController.executeRequest(BoxItem::class.java, (shareController.getCreatedSharedLinkRequest(mockBoxFile) as BoxRequestsFile.UpdatedSharedFile).setCanDownload(canDownload))).thenReturn(mockSharedLinkResponseTask)
+        whenever(shareController.executeRequest(BoxItem::class.java, shareController.getCreatedSharedLinkRequest(mockShareItem).setAccess(newAccess))).thenReturn(mockSharedLinkResponseTask)
+        whenever(shareController.executeRequest(BoxItem::class.java, shareController.getCreatedSharedLinkRequest(mockShareItem).setUnsharedAt(mockDate))).thenReturn(mockSharedLinkResponseTask)
+        whenever(shareController.executeRequest(BoxItem::class.java, shareController.getCreatedSharedLinkRequest(mockShareItem).setRemoveUnsharedAtDate())).thenReturn(mockSharedLinkResponseTask)
+        whenever(shareController.executeRequest(BoxItem::class.java, shareController.getCreatedSharedLinkRequest(mockShareItem).setPassword(newPassword))).thenReturn(mockSharedLinkResponseTask)
+        whenever(shareController.supportedFeatures).thenReturn(mockFeatureResponseTask)
+
+        whenever(shareController.fetchCollaborations(mockShareItem)).thenReturn(mockFetchCollaborationResponseTask)
+        whenever(shareController.deleteCollaboration(mockCollaboration)).thenReturn(mockDeleteCollaborationResponseTask)
+        whenever(shareController.updateCollaboration(mockCollaboration, mockSelectedRole)).thenReturn(mockUpdateCollaborationResponseTask)
+        whenever(shareController.updateOwner(mockCollaboration)).thenReturn(mockUpdateOwnerResponseTask)
+
 
         shareRepo = ShareRepo(shareController)
         createStubs()
@@ -81,32 +121,185 @@ class ShareRepoTest {
 
         doAnswer {
             val callback = it.arguments[0] as BoxFutureTask.OnCompletedListener<BoxItem>
-            callback.onCompleted(mockSharedLinksOperationResponse)
+            callback.onCompleted(mockFetchItemInfoResponse)
             null
-        }.whenever(mockSharedLinksOperationResponseTask).addOnCompletedListener(any<BoxFutureTask.OnCompletedListener<BoxItem>>())
+        }.whenever(mockFetchItemInfoResponseTask).addOnCompletedListener(any<BoxFutureTask.OnCompletedListener<BoxItem>>())
+
+        doAnswer {
+            val callback = it.arguments[0] as BoxFutureTask.OnCompletedListener<BoxItem>
+            callback.onCompleted(mockSharedLinkResponse)
+            null
+        }.whenever(mockSharedLinkResponseTask).addOnCompletedListener(any<BoxFutureTask.OnCompletedListener<BoxItem>>())
+
+        doAnswer {
+            val callback = it.arguments[0] as BoxFutureTask.OnCompletedListener<BoxIteratorCollaborations>
+            callback.onCompleted(mockFetchCollaborationResponse)
+            null
+        }.whenever(mockFetchCollaborationResponseTask).addOnCompletedListener(any<BoxFutureTask.OnCompletedListener<BoxIteratorCollaborations>>())
+
+        doAnswer {
+            val callback = it.arguments[0] as BoxFutureTask.OnCompletedListener<BoxFeatures>
+            callback.onCompleted(mockFeatureResponse)
+            null
+        }.whenever(mockFeatureResponseTask).addOnCompletedListener(any<BoxFutureTask.OnCompletedListener<BoxFeatures>>())
+
+        doAnswer {
+            val callback = it.arguments[0] as BoxFutureTask.OnCompletedListener<BoxCollaboration>
+            callback.onCompleted(mockUpdateCollaborationResponse)
+            null
+        }.whenever(mockUpdateCollaborationResponseTask).addOnCompletedListener(any<BoxFutureTask.OnCompletedListener<BoxCollaboration>>())
+
+        doAnswer {
+            val callback = it.arguments[0] as BoxFutureTask.OnCompletedListener<BoxVoid>
+            callback.onCompleted(mockUpdateOwnerResponse)
+            null
+        }.whenever(mockUpdateOwnerResponseTask).addOnCompletedListener(any<BoxFutureTask.OnCompletedListener<BoxVoid>>())
+
+        doAnswer {
+            val callback = it.arguments[0] as BoxFutureTask.OnCompletedListener<BoxVoid>
+            callback.onCompleted(mockDeleteCollaborationResponse)
+            null
+        }.whenever(mockDeleteCollaborationResponseTask).addOnCompletedListener(any<BoxFutureTask.OnCompletedListener<BoxVoid>>())
     }
 
 
     @Test
     fun `test fetch roles update LiveData values correctly`() {
-        assertNull(shareRepo.getRoleItem().value) //initially the LiveData should not have any value
+        assertNull(shareRepo.roleItem.value) //initially the LiveData should not have any value
         shareRepo.fetchRolesFromRemote(mockShareItem) //get a value and update as needed
         assertEquals(mockFetchRolesResponse, shareRepo.getRoleItem().value)
     }
 
 
     @Test
-    fun `test get invitees update LiveData values correctly`() {
-        assertNull(shareRepo.getInvitees().value) //initially the LiveData should not have any value
+    fun `test fetch invitees update LiveData values correctly`() {
+        assertNull(shareRepo.invitees.value) //initially the LiveData should not have any value
         shareRepo.fetchInviteesFromRemote(mockShareItem, mockFilter) //get a value and update as needed
         assertEquals(mockGetInviteeResponse, shareRepo.getInvitees().value)
     }
 
     @Test
-    fun `test add collabs update LiveData values correctly` () {
-        assertNull(shareRepo.getInviteCollabsBatchResponse().value) //initially the LiveData should not have any value
+    fun `test invite collabs update LiveData values correctly` () {
+        assertNull(shareRepo.inviteCollabsBatchResponse.value) //initially the LiveData should not have any value
         shareRepo.inviteCollabs(mockShareItem, mockSelectedRole, mockEmailList) //get a value and update as needed
         assertEquals(mockAddCollabsResponse, shareRepo.getInviteCollabsBatchResponse().value)
     }
+
+    @Test
+    fun `test fetch item info update LiveData values correctly` () {
+        assertNull(shareRepo.itemInfo.value) //initially the LiveData should not have any value
+        shareRepo.fetchItemInfo(mockShareItem) //get a value and update as needed
+        assertEquals(mockFetchItemInfoResponse, shareRepo.itemInfo.value)
+    }
+
+    @Test
+    fun `test create shared link update LiveData values correctly` () {
+        assertNull(shareRepo.shareLinkedItem.value) //initially the LiveData should not have any value
+
+        shareRepo.createDefaultSharedLink(mockShareItem) //get a value and update as needed
+        assertEquals(mockSharedLinkResponse, shareRepo.shareLinkedItem.value)
+    }
+
+    @Test
+    fun `test disable shared link update LiveData values correctly` () {
+        assertNull(shareRepo.shareLinkedItem.value) //initially the LiveData should not have any value
+
+        shareRepo.disableSharedLink(mockShareItem) //get a value and update as needed
+        assertEquals(mockSharedLinkResponse, shareRepo.shareLinkedItem.value)
+    }
+
+    @Test
+    fun `test change download permission update LiveData values correctly` () {
+        assertNull(shareRepo.shareLinkedItem.value) //initially the LiveData should not have any value
+
+        shareRepo.changeDownloadPermission(mockBoxFile, canDownload) //get a value and update as needed
+        assertEquals(mockSharedLinkResponse, shareRepo.shareLinkedItem.value)
+    }
+
+    @Test
+    fun `test change download permission illegal argument` () {
+        assertNull(shareRepo.shareLinkedItem.value) //initially the LiveData should not have any value
+        try {
+            shareRepo.changeDownloadPermission(mockShareItem, canDownload)
+        } catch (e: Exception) {
+            assertTrue(e is IllegalArgumentException) //illegal argument exception should occur
+        }
+    }
+
+    @Test
+    fun `test change access level update LiveData values correctly` () {
+        assertNull(shareRepo.shareLinkedItem.value) //initially the LiveData should not have any value
+
+        shareRepo.changeAccessLevel(mockBoxFile, newAccess) //get a value and update as needed
+        assertEquals(mockSharedLinkResponse, shareRepo.shareLinkedItem.value)
+    }
+
+
+    @Test
+    fun `test set expiry date update LiveData values correctly` () {
+        assertNull(shareRepo.shareLinkedItem.value) //initially the LiveData should not have any value
+
+        shareRepo.setExpiryDate(mockBoxFile, mockDate) //get a value and update as needed
+        assertEquals(mockSharedLinkResponse, shareRepo.shareLinkedItem.value)
+    }
+
+
+    @Test
+    fun `test remove expiry date update LiveData values correctly` () {
+        assertNull(shareRepo.shareLinkedItem.value) //initially the LiveData should not have any value
+
+        shareRepo.removeExpiryDate(mockBoxFile) //get a value and update as needed
+        assertEquals(mockSharedLinkResponse, shareRepo.shareLinkedItem.value)
+    }
+
+    @Test
+    fun `test change password update LiveData values correctly` () {
+        assertNull(shareRepo.shareLinkedItem.value) //initially the LiveData should not have any value
+
+        shareRepo.changePassword(mockShareItem, newPassword) //get a value and update as needed
+        assertEquals(mockSharedLinkResponse, shareRepo.shareLinkedItem.value)
+    }
+
+    @Test
+    fun `test fetch features update LiveData values correctly` () {
+        assertNull(shareRepo.supportFeatures.value) //initially the LiveData should not have any value
+
+        shareRepo.fetchSupportedFeatures() //get a value and update as needed
+        assertEquals(mockFeatureResponse, shareRepo.supportFeatures.value)
+    }
+
+    @Test
+    fun `test fetch collaborations update LiveData values correctly` () {
+        assertNull(shareRepo.collaborations.value) //initially the LiveData should not have any value
+
+        shareRepo.fetchCollaborations(mockShareItem) //get a value and update as needed
+        assertEquals(mockFetchCollaborationResponse, shareRepo.collaborations.value)
+    }
+
+    @Test
+    fun `test update collaborations update LiveData values correctly` () {
+        assertNull(shareRepo.updateCollaboration.value) //initially the LiveData should not have any value
+
+        shareRepo.updateCollaboration(mockCollaboration, mockSelectedRole) //get a value and update as needed
+        assertEquals(mockUpdateCollaborationResponse, shareRepo.updateCollaboration.value)
+    }
+
+    @Test
+    fun `test update owner update LiveData values correctly` () {
+        assertNull(shareRepo.updateOwner.value) //initially the LiveData should not have any value
+
+        shareRepo.updateOwner(mockCollaboration) //get a value and update as needed
+        assertEquals(mockUpdateOwnerResponse, shareRepo.updateOwner.value)
+    }
+
+    @Test
+    fun `test delete collaboration update LiveData values correctly` () {
+        assertNull(shareRepo.deleteCollaboration.value) //initially the LiveData should not have any value
+
+        shareRepo.deleteCollaboration(mockCollaboration) //get a value and update as needed
+        assertEquals(mockDeleteCollaborationResponse, shareRepo.deleteCollaboration.value)
+    }
+
+
 
 }
