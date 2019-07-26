5c4d7d8a5f938e844a9f121abc50106545efdcee
myothiha09 <myothiha1997@gmail.com>
2019-07-26 15:00:22 -0700

    removed comments

--- box-share-sdk/src/main/java/com/box/androidsdk/share/usx/fragments/InviteCollaboratorsFragment.java
+++ box-share-sdk/src/main/java/com/box/androidsdk/share/usx/fragments/InviteCollaboratorsFragment.java
@@ -45,14 +45,6 @@ import com.tokenautocomplete.TokenCompleteTextView;
 
 import java.util.List;
 
-/**
- * Fragment to let users invite collaborators on an item.
- *
- * There are two listeners used here:
- * 1. InviteCollaboratorsListener is used to set up a listener by the parent Activity or Fragment on this Fragment.
- * 2. ShowCollaboratorsListener is used to set up a listener by this fragment on the child custom view called CollaboratorsInitialsView.
- */
-
 public class InviteCollaboratorsFragment extends BoxFragment implements TokenCompleteTextView.TokenListener<BoxInvitee> {
 
     private static final Integer MY_PERMISSIONS_REQUEST_READ_CONTACTS = 32;
